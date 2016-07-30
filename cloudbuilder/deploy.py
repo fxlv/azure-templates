18cf9c4ffae5ed76754e6267d66c954c2bc610d0
Kaspars Mickevics <kaspars@fx.lv>
2016-07-30 10:53:01 +0300

    cleanup

--- cloudbuilder/deploy.py
+++ cloudbuilder/deploy.py
@@ -14,15 +14,6 @@ resource_group_name = "lalala3"
 template_file_path = "template.json"
 parameters_file_path = "parameters.json"
 
-# first check what vms already exist
-# the flow goes like this:
-# * create AzureVM object
-# * create VM
-# ** check if subscription id is provided and check if it's valid,
-#    set it as the default
-# ** check if resource group already exists, create it if it doesn't
-# ** create the VM
-
 a = vm.AzureVM(subscription_id)
 a.create_vm(deployment_name, resource_group_name, resource_group_location,
             template_file_path, parameters_file_path)
